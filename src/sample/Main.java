d9cd7debda12537e93a6fe55833281e31af466a9
盛滉佑 <kosuke@kosuke-mbp.local>
2019-06-16 10:36:05 +0900

    音源再生

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,35 @@
 package sample;
 
+import java.io.File;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.media.AudioClip;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("JavaFX");
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane, 320, 240);
+        Button b = new Button("音源再生");
+        b.addEventHandler( MouseEvent.MOUSE_CLICKED ,  e -> buttonCliked( e ) );
+        pane.setCenter(b);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void buttonCliked( MouseEvent e ){
+        AudioClip media = new AudioClip(new File("sample.wav").toURI().toString());
+        media.play();
     }
 }
