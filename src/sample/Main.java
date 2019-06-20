639a6ec6f03402ef3bad8511b43d30bd041d7a4f
盛滉佑 <kosuke@kosuke-mbp.local>
2019-06-20 13:35:29 +0900

    時刻による音源変更（借）

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,10 +15,13 @@ import javafx.stage.Stage;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static java.lang.Thread.*;
+
 public class Main extends Application {
     MediaPlayer mplayer = null;
     Slider slider = null;
-    Label l = null;
+    Label label = null;
+    String PATH = null;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -29,11 +32,6 @@ public class Main extends Application {
         // 現在の日付のみを取得
         LocalDate d1 = LocalDate.now();
         System.out.println(d1);
-        // 現在の時刻のみを取得
-        LocalTime t1 = LocalTime.now();
-        System.out.println(t1);
-        int timeHour = t1.getHour();
-        System.out.println(timeHour);
 
         stage.setTitle("JavaFX");
         VBox pane = new VBox();
@@ -45,35 +43,101 @@ public class Main extends Application {
         Button pauseButton = new Button("一時停止");
         pauseButton.addEventHandler( MouseEvent.MOUSE_CLICKED ,  f -> buttonClikedB( f ) );
         //ラベルの設定
-        l = new Label("");
+        label = new Label("");
         //スライダーの初期化
         slider = new Slider();
 
         pane.getChildren().add( playButton );
         pane.getChildren().add( pauseButton );
         pane.getChildren().add( slider );
-        pane.getChildren().add( l );
+        pane.getChildren().add( label );
         stage.setScene(scene);
         stage.show();
     }
 
     private void buttonClikedA( MouseEvent e ){
-        Media media = new Media(new File("sample.wav").toURI().toString());
-        mplayer = new MediaPlayer( media );
-        mplayer.play();
+        // 現在の時刻のみを取得
+        LocalTime t1 = LocalTime.now();
+        int timeHour = t1.getHour();
 
+        //音源のPATHを取得
+        PATH = "audio/sample" + new String(String.valueOf(timeHour)) + ".wav";
+        Media mediaA = new Media(new File(PATH).toURI().toString());
+
+        mplayer = new MediaPlayer(mediaA);
+        mplayer.play();
         mplayer.setOnReady(() -> {
             //スライダーの最大最小を設定
             slider.setMin(mplayer.getStartTime().toSeconds());
             slider.setMax(mplayer.getStopTime().toSeconds());
-            //音声の長さを表示
-            l.setText( "再生時間 " + mplayer.getStopTime().toSeconds() + " 秒" );
+            //音源のPATHを設定
+            label.setText(PATH);
             //ループ回数を無限に指定
             mplayer.setCycleCount(MediaPlayer.INDEFINITE);
         });
+        mplayer.setOnRepeat(() -> {
+            LocalTime t2 = LocalTime.now();
+            int timeMinute = t2.getMinute();
+            if(timeMinute == 59){
+                mplayer.stop();
+            }
+        });
+        mplayer.setOnStopped(() -> {
+            // 現在の時刻のみを取得
+            LocalTime t2 = LocalTime.now();
+            int timeHourA = t2.getHour() + 1;
+            //音源のPATHを取得
+            PATH = "audio/sample" + new String(String.valueOf(timeHourA)) + ".wav";
+            Media mediaB = new Media(new File(PATH).toURI().toString());
 
+            mplayer = new MediaPlayer(mediaB);
+            mplayer.play();
+            mplayer.setOnReady(() -> {
+                //スライダーの最大最小を設定
+                slider.setMin(mplayer.getStartTime().toSeconds());
+                slider.setMax(mplayer.getStopTime().toSeconds());
+                //音源のPATHを設定
+                label.setText(PATH);
+                //ループ回数を無限に指定
+                mplayer.setCycleCount(MediaPlayer.INDEFINITE);
+            });
+        });
         //再生時間が進んだらスライダーの位置を変更する
-        mplayer.currentTimeProperty().addListener((Observable observable) -> slider.setValue( mplayer.getCurrentTime().toSeconds() ));
+        mplayer.currentTimeProperty().addListener((Observable observable) -> slider.setValue(mplayer.getCurrentTime().toSeconds()));
+
+
+//        for(;;) {
+//            //System.out.println(t1);
+//            timeHour = t1.getHour();
+//            int timeMinute = t1.getMinute();
+//            //System.out.println(timeHour);
+//            if (timeMinute == 59) {
+//                timeHour++;
+//
+//                //音源のPATHを取得
+//                PATH = "audio/sample" + new String(String.valueOf(timeHour)) + ".wav";
+//                Media mediaB = new Media(new File(PATH).toURI().toString());
+//
+//                mplayer = new MediaPlayer(mediaB);
+//                mplayer.play();
+//                mplayer.setOnReady(() -> {
+//                    //スライダーの最大最小を設定
+//                    slider.setMin(mplayer.getStartTime().toSeconds());
+//                    slider.setMax(mplayer.getStopTime().toSeconds());
+//                    //音源のPATHを設定
+//                    label.setText(PATH);
+//                    //ループ回数を無限に指定
+//                    mplayer.setCycleCount(MediaPlayer.INDEFINITE);
+//                });
+//                //再生時間が進んだらスライダーの位置を変更する
+//                mplayer.currentTimeProperty().addListener((Observable observable) -> slider.setValue(mplayer.getCurrentTime().toSeconds()));
+//            }
+//            try {
+//                sleep(6000);
+//            } catch (InterruptedException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
     }
 
     private void buttonClikedB( MouseEvent f ){
